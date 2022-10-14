80a56bfea7db5fa3a329b78d21fa836d924a30aa
Steppe Schoolkate <S.Schoolkate@hotmail.nl>
2022-10-14 15:57:16 +0200

    Added health and obstacle collision to player

--- EndlessRunner/Assets/_Scripts/Player.cs
+++ EndlessRunner/Assets/_Scripts/Player.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 namespace _Scripts
@@ -7,16 +8,27 @@ namespace _Scripts
         [SerializeField] private LevelSettings settings;
         
         [SerializeField] private float moveSpeed;
+        [SerializeField] private int baseHealth;
 
+        private int _health;
         private Vector3 _origin;
         private int _position;
 
         private int Position
         {
             get => _position;
-            set => _position = Mathf.Clamp(value, 0, settings.Lanes);
+            set => _position = Mathf.Clamp(value, 0, settings.Lanes - 1);
         }
 
+        public int Health
+        {
+            get => _health;
+            set
+            {
+                _health = value;
+                if (_health <= 0) Die();
+            }
+        }
 
         private Controls _controls;
         private Controls Controls
@@ -29,6 +41,11 @@ namespace _Scripts
                 }
                 return _controls = new Controls();
             }
+        }
+
+        private void Awake()
+        {
+            _health = baseHealth;
         }
 
         private void Start()
@@ -46,6 +63,20 @@ namespace _Scripts
         private void Update()
         {
             transform.position = Vector3.Lerp(transform.position, _origin + Vector3.right * (_position * settings.LaneMargin), Time.deltaTime * moveSpeed);
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            var laneObject = other.GetComponent<LaneObject>();
+            if (laneObject)
+            {
+                laneObject.OnCollisionWithPlayer(this);
+            }
+        }
+
+        private void Die()
+        {
+            Destroy(gameObject);
         }
     }
 }
